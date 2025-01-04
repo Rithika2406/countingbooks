1b2b17c976e6c6d05065394d22c80ba31122f291
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2025-01-04 12:30:34 +0530

    Add files via upload

---
+++ countingbooks.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int t=S.nextInt();
+		while(t-->0)
+		{
+		    int x=S.nextInt();
+		    int a=(x*1000)/100;
+		    System.out.println(a);
+		}
+
+	}
+}
